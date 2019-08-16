ef2a204ca035e02282400b82b37af63e196b9d10
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-08-16 20:46:23 +0200

    Add extensions to nimbox

---
+++ src/nimboxext.nim
@@ -0,0 +1,29 @@
+## Extension templates for the nimbox library
+##
+## These functions help in toggling the 256-colors option
+
+import nimbox
+
+template newNb*(): Nimbox =
+  ## Wrapper for `newNimbox`
+  let nb = newNimbox()
+  if colors256Mode():
+    nb.outputMode = out256
+  nb
+
+template colors256Mode*(): bool =
+  ## Should the 256-colors mode be turned on?
+  existsEnv("NIMMM_256")
+
+template c8*(color:int): int =
+  ## Convert this color (`ck8`) into
+  ## an int with regards to the current color mode
+  if colors256Mode():
+    color - 1
+  else:
+    color
+
+template c8*(color:Color): int =
+  ## Convert this color enum into an int
+  ## with regards to the current color mode
+  c8(ord(color))
