27298a4914ddb1007c1e86a6a4ee9a55db93ad57
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-02-25 08:51:29 +0100

    wip keymap

--- src/keymap.nim
+++ src/keymap.nim
@@ -45,23 +45,36 @@ type
     mouse: TableRef[Mouse, Action]
 
 let
+  actionNames = {"quit":AcQuit, "shell":AcShell, "toggle-hidden":AcToggleHidden,
+                  "select-all":AcSelectAll, "clear-selection":AcClearSelection,
+                  "first":AcFirst, "last":AcLast, "down":AcDown,
+                  "up":AcUp, "left":AcLeft, "right":AcRight,
+                  "home":AcHomeDir, "new-tab":AcNewTab, "close-tab":AcCloseTab,
+                  "tab-1":AcTab1, "tab-2":AcTab2, "tab-3":AcTab3,
+                  "tab-4":AcTab4, "tab-5":AcTab5, "tab-6":AcTab6,
+                  "tab-7":AcTab7, "tab-8":AcTab8, "tab-9":AcTab9,
+                  "tab-10":AcTab10, "edit":AcEdit, "pager":AcPager, "rename":AcRename,
+                  "new-file":AcNewFile, "new-dir":AcNewDir, "copy":AcCopySelected,
+                  "move":AcMoveSelected, "delete":AcDeleteSelected,
+                  "search":AcSearch,
+                }.newTable
   defaultChars = {'q':AcQuit, '!':AcShell, '.':AcToggleHidden,
-                 'a':AcSelectAll, 's':AcClearSelection,
-                 'g':AcFirst, 'G':AcLast, 'j':AcDown,
-                 'k':AcUp, 'h':AcLeft, 'l':AcRight,
-                 '~':AcHomeDir, 't':AcNewTab, 'w':AcCloseTab,
-                 '1':AcTab1, '2':AcTab2, '3':AcTab3,
-                 '4':AcTab4, '5':AcTab5, '6':AcTab6,
-                 '7':AcTab7, '8':AcTab8, '9':AcTab9,
-                 '0':AcTab10, 'e':AcEdit, 'p':AcPager, 'r':AcRename,
-                 'f':AcNewFile, 'd':AcNewDir, 'P':AcCopySelected,
-                 'V':AcMoveSelected, 'X':AcDeleteSelected,
-                 '/':AcSearch,
-                 }.newTable
+                   'a':AcSelectAll, 's':AcClearSelection,
+                   'g':AcFirst, 'G':AcLast, 'j':AcDown,
+                   'k':AcUp, 'h':AcLeft, 'l':AcRight,
+                   '~':AcHomeDir, 't':AcNewTab, 'w':AcCloseTab,
+                   '1':AcTab1, '2':AcTab2, '3':AcTab3,
+                   '4':AcTab4, '5':AcTab5, '6':AcTab6,
+                   '7':AcTab7, '8':AcTab8, '9':AcTab9,
+                   '0':AcTab10, 'e':AcEdit, 'p':AcPager, 'r':AcRename,
+                   'f':AcNewFile, 'd':AcNewDir, 'P':AcCopySelected,
+                   'V':AcMoveSelected, 'X':AcDeleteSelected,
+                   '/':AcSearch,
+                }.newTable
   defaultSymbols = {Symbol.Enter:AcRight, Symbol.Backspace:AcLeft,
-                    Symbol.Space:AcSelect, Symbol.Up:AcUp,
-                    Symbol.Down:AcDown, Symbol.Left:AcLeft,
-                    Symbol.Right:AcRight}.newTable
+                     Symbol.Space:AcSelect, Symbol.Up:AcUp,
+                     Symbol.Down:AcDown, Symbol.Left:AcLeft,
+                     Symbol.Right:AcRight}.newTable
   defaultMouse = {Mouse.WheelUp:AcUp, Mouse.WheelDown:AcDown}.newTable
   defaultKeymap* = Keymap(chars:defaultChars, symbols:defaultSymbols, mouse:defaultMouse)
 
