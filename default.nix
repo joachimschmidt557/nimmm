4e7b7c595e7d0af93cab751ea375ca591f55b892
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-06-14 22:00:57 +0200

    pin dependencies

--- default.nix
+++ default.nix
@@ -1,28 +1,28 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {} }:
 
 let
-  noise = fetchFromGitHub {
+  noise = pkgs.fetchFromGitHub {
     owner = "jangko";
     repo = "nim-noise";
-    rev = "master";
-    sha256 = "1bg8vjirf87526pk8ii8n3kp8f2fffhlcs1mdh1ahljcw8zbnq1k";
+    rev = "db1e86e312413e4348fa82c02340784316a89cc1";
+    sha256 = "0n9l2dww5smrsl1xfqxjnxz3f1srb72lc1wl3pdvs6xfyf44qzlh";
   };
 
-  nimbox = fetchFromGitHub {
+  nimbox = pkgs.fetchFromGitHub {
     owner = "dom96";
     repo = "nimbox";
-    rev = "master";
+    rev = "6a56e76c01481176f16ae29b7d7c526bd83f229b";
     sha256 = "15x1sdfxa1xcqnr68705jfnlv83lm0xnp2z9iz3pgc4bz5vwn4x1";
   };
 
-  lscolors = fetchFromGitHub {
+  lscolors = pkgs.fetchFromGitHub {
     owner = "joachimschmidt557";
     repo = "nim-lscolors";
     rev = "v0.3.3";
     sha256 = "0526hqh46lcfsvymb67ldsc8xbfn24vicn3b8wrqnh6mag8wynf4";
   };
 
-in stdenv.mkDerivation rec {
+in pkgs.stdenv.mkDerivation rec {
   pname = "nimmm";
   version = "0.1.2";
 
@@ -34,8 +34,8 @@ in stdenv.mkDerivation rec {
   #   sha256 = "1zpq181iz6g7yfi298gjwv33b89l4fpnkjprimykah7py5cpw67w";
   # };
 
-  nativeBuildInputs = [ nim ];
-  buildInputs = [ termbox pcre ];
+  nativeBuildInputs = with pkgs; [ nim ];
+  buildInputs = with pkgs; [ termbox pcre ];
 
   NIX_LDFLAGS = "-lpcre";
 
