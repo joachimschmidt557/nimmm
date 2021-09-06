18764438375c1a4b6a47e4bc4193d9f652f0db51
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-09-06 10:22:16 +0200

    Use dependencies as flake inputs

--- flake.nix
+++ flake.nix
@@ -1,33 +1,25 @@
 {
   description = "Terminal file manager written in nim";
 
-  outputs = { self, nixpkgs }: {
+  inputs.noise = {
+    url = "github:jangko/nim-noise/v0.1.14";
+    flake = false;
+  };
+
+  inputs.nimbox = {
+    url = "github:dom96/nimbox";
+    flake = false;
+  };
+
+  inputs.lscolors = {
+    url = "github:joachimschmidt557/nim-lscolors/v0.3.3";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, noise, nimbox, lscolors }: {
 
     packages.x86_64-linux.nimmm =
       with import nixpkgs { system = "x86_64-linux"; };
-      let
-        noise = pkgs.fetchFromGitHub {
-          owner = "jangko";
-          repo = "nim-noise";
-          rev = "v0.1.14";
-          sha256 = "0wndiphznfyb1pac6zysi3bqljwlfwj6ziarcwnpf00sw2zni449";
-        };
-
-        nimbox = pkgs.fetchFromGitHub {
-          owner = "dom96";
-          repo = "nimbox";
-          rev = "6a56e76c01481176f16ae29b7d7c526bd83f229b";
-          sha256 = "15x1sdfxa1xcqnr68705jfnlv83lm0xnp2z9iz3pgc4bz5vwn4x1";
-        };
-
-        lscolors = pkgs.fetchFromGitHub {
-          owner = "joachimschmidt557";
-          repo = "nim-lscolors";
-          rev = "v0.3.3";
-          sha256 = "0526hqh46lcfsvymb67ldsc8xbfn24vicn3b8wrqnh6mag8wynf4";
-        };
-
-      in
       stdenv.mkDerivation {
         pname = "nimmm";
         version = "master";
