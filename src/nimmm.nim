joachimschmidt557/nimmm

commit 35ab9369275aa700003f92f11383dd5a8974f1bc

restore resetTab() on left
without resetTab, the current search query and search state would be
preserved

src/nimmm.nim
@@ -88,6 +88,7 @@ proc left(s: var State, lsc: LsColors) =
   let prevDir = os.getCurrentDir()
   s.safeSetCurDir(parentDir(paths.getCurrentDir()))
   s.rescan(lsc)
+  s.resetTab()
   s.currentIndex = getIndexOfItem(s, prevDir)
 
 proc right(s: var State, lsc: LsColors) =

commit fe2cc123ffc8743ef701db482905bc08118f8a24

resetTab() before rescan on left action
This fixes an issue introduced in
35ab9369275aa700003f92f11383dd5a8974f1bc

src/nimmm.nim
@@ -87,8 +87,8 @@ proc left(s: var State, lsc: LsColors) =
     return
   let prevDir = os.getCurrentDir()
   s.safeSetCurDir(parentDir(paths.getCurrentDir()))
-  s.rescan(lsc)
   s.resetTab()
+  s.rescan(lsc)
   s.currentIndex = getIndexOfItem(s, prevDir)
 
 proc right(s: var State, lsc: LsColors) =