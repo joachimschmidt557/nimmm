d7660a5802358718a4021de47fcc206ace77e77f
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-05-24 18:01:41 +0200

    move readline operation to separate file
    also adds handling for Home and End keys

--- src/nimmm.nim
+++ src/nimmm.nim
@@ -4,7 +4,7 @@ import std/[os, sets, parseopt, sequtils, algorithm, strutils,
 import nimbox
 import lscolors
 
-import core, scan, draw, external, nimboxext, keymap
+import core, scan, draw, external, nimboxext, keymap, readline
 
 proc getIndexOfItem(s: State, name: string): int =
   let
@@ -120,51 +120,6 @@ template withoutNimbox*(nb: var Nimbox, enable256Colors: bool, body: untyped) =
   nb.shutdown()
   body
   nb = newNb(enable256Colors)
-
-type
-  ProcessInputTextModeResult = enum
-    PrNoAction,
-    PrCanceled,
-    PrComplete,
-
-proc processInputTextMode(event: nimbox.Event,
-                          input: var string,
-                          cursorPos: var int): ProcessInputTextModeResult =
-  ## common input processing for MdInputText and MdSearch
-  case event.kind
-  of EventType.Key:
-    case event.sym
-    of Symbol.Escape:
-      return PrCanceled
-    of Symbol.Backspace:
-      if cursorPos > 0:
-        let (_, runeLen) = lastRune(input, cursorPos - 1)
-        input = input[0..cursorPos - 1 - runeLen] & input.substr(cursorPos)
-        cursorPos -= runeLen
-    of Symbol.Enter:
-      return PrComplete
-    of Symbol.Space:
-      let inserted = " "
-      input.insert(inserted, cursorPos)
-      cursorPos += inserted.len
-    of Symbol.Character:
-      let inserted = $event.ch.Rune
-      input.insert(inserted, cursorPos)
-      cursorPos += inserted.len
-    of Symbol.Left:
-      if cursorPos > 0:
-        let (_, runeLen) = lastRune(input, cursorPos - 1)
-        cursorPos -= runeLen
-    of Symbol.Right:
-      if cursorPos < input.len:
-        let runeLen = runeLenAt(input, cursorPos)
-        cursorPos += runeLen
-    else:
-      discard
-  of EventType.Mouse, EventType.Resize, EventType.None:
-    discard
-
-  return PrNoAction
 
 proc mainLoop(nb: var Nimbox, enable256Colors: bool) =
   let
---
+++ src/readline.nim
@@ -0,0 +1,52 @@
+import std/unicode
+
+import nimbox
+
+type
+  ProcessInputTextModeResult* = enum
+    PrNoAction,
+    PrCanceled,
+    PrComplete,
+
+proc processInputTextMode*(event: nimbox.Event,
+                          input: var string,
+                          cursorPos: var int): ProcessInputTextModeResult =
+  ## common input processing for MdInputText and MdSearch
+  case event.kind
+  of EventType.Key:
+    case event.sym
+    of Symbol.Escape:
+      return PrCanceled
+    of Symbol.Backspace:
+      if cursorPos > 0:
+        let (_, runeLen) = lastRune(input, cursorPos - 1)
+        input = input[0..cursorPos - 1 - runeLen] & input.substr(cursorPos)
+        cursorPos -= runeLen
+    of Symbol.Enter:
+      return PrComplete
+    of Symbol.Space:
+      let inserted = " "
+      input.insert(inserted, cursorPos)
+      cursorPos += inserted.len
+    of Symbol.Character:
+      let inserted = $event.ch.Rune
+      input.insert(inserted, cursorPos)
+      cursorPos += inserted.len
+    of Symbol.Left:
+      if cursorPos > 0:
+        let (_, runeLen) = lastRune(input, cursorPos - 1)
+        cursorPos -= runeLen
+    of Symbol.Right:
+      if cursorPos < input.len:
+        let runeLen = runeLenAt(input, cursorPos)
+        cursorPos += runeLen
+    of Symbol.Home:
+      cursorPos = 0;
+    of Symbol.End:
+      cursorPos = input.len;
+    else:
+      discard
+  of EventType.Mouse, EventType.Resize, EventType.None:
+    discard
+
+  return PrNoAction
